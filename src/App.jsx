b5a23346dee3999df62289d591d6d89ed35f6142
hasanulhasan <hasanlink71@yahoo.com>
2024-05-10 15:02:53 +0600

    UI converted to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Footer from "./component/Footer";
+import LeftSideBar from "./component/LeftSideBar";
+import Navbar from "./component/Navbar";
+import RightSideBar from "./component/RightSideBar";
 
 function App() {
-
   return (
     <>
-      <h1>Project setup ready from another pc</h1>
+      <Navbar/>
+      <main className="my-10 lg:my-14">
+        <div className="container mx-auto grid grid-cols-12 gap-8">
+          <LeftSideBar/>
+          <RightSideBar/>
+        </div>
+      </main>
+      <Footer/>
     </>
   );
 }
